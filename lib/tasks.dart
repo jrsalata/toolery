5335627c27047c36112c119e2684dbbeaed87657
jrsalata <jsalata90@gmail.com>
2026-01-10 23:37:17 +0000

    Moved task list to other widget

--- lib/tasks.dart
+++ lib/tasks.dart
@@ -4,38 +4,49 @@ import 'package:flutter/material.dart';
 import 'package:toolery/models/task.dart';
 
 class TaskPage extends StatelessWidget {
-  TaskPage({super.key});
-  
-  final Future<List<Task>> tasks = allTasks();
+  const TaskPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Tasks')),
-        body: Center(
-          child: FutureBuilder<List<Task>>(
-            future: tasks,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState != ConnectionState.done) {
-                return const CircularProgressIndicator();
-              }
-              if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              }
-              final tasks = snapshot.data ?? [];
-              return tasks.isNotEmpty ? ListView(
-                children: [
-                  for (Task task in tasks)
-                    ListTile(
-                      title: Text(task.name),
-                      subtitle: Text(task.description),
-                    )
-                ],
-              ) : Text("No tasks yet :(");
-            },
-          ),
-        ),
+        body: TaskList(),
+      ),
+    );
+  }
+}
+
+// dedicated Widget to list all of the tasks
+class TaskList extends StatelessWidget {
+  TaskList({super.key});
+  final Future<List<Task>> tasks = allTasks();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FutureBuilder<List<Task>>(
+        future: tasks,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const CircularProgressIndicator();
+          }
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+          final tasks = snapshot.data ?? [];
+          return tasks.isNotEmpty
+              ? ListView(
+                  children: [
+                    for (Task task in tasks)
+                      ListTile(
+                        title: Text(task.name),
+                        subtitle: Text(task.description),
+                      ),
+                  ],
+                )
+              : Text("No tasks yet :(");
+        },
       ),
     );
   }
